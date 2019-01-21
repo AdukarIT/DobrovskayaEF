9939b6d20be1cce2167138981eb1a7ceed039d41
DobrovskayaElena <44357557+DobrovskayaElena@users.noreply.github.com>
2019-01-21 19:29:30 +0300

    changed

--- tasks2/myscript.js
+++ tasks2/myscript.js
@@ -1,23 +1,21 @@
-    /*
-
-Task 1.
+/*Task 1.
 
     let flatNumbers = +prompt("Введите номер квартиры");
-    if (flatNumbers <= 0 || flatNumbers > 100) {
-    	console.log("Неверный номер квартиры!");
-    	alert("Неверный номер квартиры!");
+    if (flatNumbers < 1 || flatNumbers > 100) {
+        console.log("Неверный номер квартиры!");
+        alert("Неверный номер квартиры!");
     } else if (flatNumbers >= 1 && flatNumbers <=20) {
-    	console.log(1);
+        console.log(1);
     } else if (flatNumbers >= 21 && flatNumbers <=40) {
-    	console.log(2);
+        console.log(2);
     } else if (flatNumbers >= 41 && flatNumbers <=60) {
-    	console.log(3);
+        console.log(3);
     }
     else if (flatNumbers >= 61 && flatNumbers <=80) {
-    	console.log(4);
+        console.log(4);
     }
     else if (flatNumbers >= 81 && flatNumbers <=100) {
-    	console.log(5);
+        console.log(5);
     }
 
 Task 2.
@@ -60,7 +58,7 @@ switch (autoCountries) {
     console.log(autoCountries + "\nSweden");
     break;
     default:
-    console.log("some_input - страна происхождения неизвестна");
+    console.log(autoCountries + "\nСтрана происхождения неизвестна");
 }
 
 Task 3.
@@ -68,28 +66,28 @@ Task 3.
 let leapYearOrNo = prompt("Введите номер года");
 if (leapYearOrNo <= 0) {
         console.log("Год указан неверно");
-} else if (leapYearOrNo % 100 == 0 && leapYearOrNo % 400 !== 0) {
-    console.log("Это не високосный год");
-    } else if (leapYearOrNo % 4 == 0 || leapYearOrNo % 400 == 0) {
+} else if (leapYearOrNo % 4 == 0 && leapYearOrNo % 100 != 0 || leapYearOrNo % 400 == 0) {
     console.log("Это високосный год");
 } else {
-	console.log("Это не високосный год");
+    console.log("Это не високосный год");
 }
 
 Task 4.
 
 let twentyTimesTable = +prompt("Введите число от 1 до 20");
+
 for (let i = 1; i <= 10; i++) {
-	if (twentyTimesTable > 0 && twentyTimesTable <=20) {
-	console.log(twentyTimesTable + "x" + i + "=" + twentyTimesTable * i);
+    if (twentyTimesTable > 0 && twentyTimesTable <=20) {
+    console.log(twentyTimesTable + "x" + i + "=" + twentyTimesTable * i);
+} else {
+    console.log("Введенное число меньше 1 или больше 20");
 }
 }
 
 Task 5.
 
 let totalSumm = 0;
-
-for (let x = 0; x <=50; x++) {
+for (let x = 1; x <=50; x++) {
     if (x % 2 == 0) continue;
     totalSumm = totalSumm + x;
 }
@@ -99,7 +97,6 @@ Task 6.
 
 let a = 0;
 let b = 1;
-
 console.log(a);
 console.log(b);
 for (let i = 3; i <= 15; i++) {
@@ -111,19 +108,14 @@ for (let i = 3; i <= 15; i++) {
 
 Task 7.
 
-let a = " ";
-let b = "#";
-let x = a + b;
-
-for (let c = 0; c <= 7; c++) {
-    for (let d = 0; d <= 2; d++) {
-        x += x;
+for (let i = 1; i <= 8; i++) {
+    let str = "";
+    for (let j = 1; j <= 8; j++) {
+        if ((i + j) % 2) {
+        str += "#";
+    } else {
+        str += " ";
     }
-    console.log(x);
-     if (c % 2 !== 0) {
-        x = a + b;
 }
-    else {
-        x = b + a;
-  }
+console.log(str);
 }*/
