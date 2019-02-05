3e846585719d3c367046d7412cfb03afdde8e75c
DobrovskayaElena <44357557+DobrovskayaElena@users.noreply.github.com>
2019-02-05 21:47:29 +0300

    tasks 5 added

---
+++ tasks5array/myscript5.js
@@ -0,0 +1,68 @@
+/*Task 1.
+
+function getRandomArray(len) {
+	let array = [0.1, 0.2, 0.3, 1, 3, 5, 4, 8, 12, 18, 25, 50];
+	for (let i = 0; i < len; i++) {
+		array[i] = Math.floor(Math.random());
+		return array;
+
+	}
+} 
+console.log(getRandomArray(12));
+
+Task 2.
+
+let averageArithmetic;
+
+function average(len) {
+	getRandomArray(12);
+	if (len == 0) {
+		return 0;
+	}
+	let sum = 0;
+	for (let i = 0; i < len; i++) {
+		sum += Math.floor(Math.random() * len);
+		averageArithmetic = sum / len;
+	}
+	return averageArithmetic;
+}
+console.log(average(12));
+
+
+Task 3.
+
+function minOneTwo(array) {
+	let numbersMin = array.sort(function(a, b){
+  	return a - b;
+  }
+ )
+  console.log("Наименьшие числа в массиве: " + array[0] + " и " + array[1]);
+}
+minOneTwo(getRandomArray(12));
+
+Task 4.
+
+function remove(array) {
+  getRandomArray(12);
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] < 0.3) {
+    array[i] = 0;
+    }
+  }
+  return array;
+}
+console.log(remove(getRandomArray(12)));
+
+Task 5.
+
+function sum(array1, array2) {
+	let array3 = new Array(array1.length);
+	for (let i = 0; i < array1.length; i++) {
+		array3[i] = array1[i] + array2[array1.length - 1 - i];
+	}
+	return array3;
+}
+console.log(sum(getRandomArray(12), getRandomArray(12)));
+
+
+
