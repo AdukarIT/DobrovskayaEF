0ba5462ef9e9b56fe04c38b9c58b97e290e0f5c9
DobrovskayaElena <44357557+DobrovskayaElena@users.noreply.github.com>
2019-02-06 07:31:11 +0300

    corrected

--- tasks5array/myscript5.js
+++ tasks5array/myscript5.js
@@ -1,9 +1,9 @@
 /*Task 1.
 
 function getRandomArray(len) {
-	let array = [0.1, 0.2, 0.3, 1, 3, 5, 4, 8, 12, 18, 25, 50];
+	let array = [];
 	for (let i = 0; i < len; i++) {
-		array[i] = Math.floor(Math.random());
+		array[i] = Math.floor(Math.random() * len);
 		return array;
 
 	}
