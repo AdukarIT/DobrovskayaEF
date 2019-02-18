9ad23f56205a47d7933fd833fd07d30a8e1da9ce
DobrovskayaElena <44357557+DobrovskayaElena@users.noreply.github.com>
2019-02-18 19:32:41 +0300

    task2 to tasks10 added

--- tasks10/myscript10.js
+++ tasks10/myscript10.js
@@ -34,4 +34,53 @@ function clone() {
 	let cloneButton = input.cloneNode(true);
 	parent.appendChild(cloneButton); 
 };
+
+
+Task 2.
+
+function createTable() {
+table = document.getElementById("newtable"); 
+table.setAttribute("border", 1);
+tr = document.createElement("TR"); 
+td = document.createElement("TD"); 
+td.setAttribute("rowspan", 1);  
+td.appendChild(document.createTextNode("1 ячейка")); 
+tr.appendChild(td); 
+td = document.createElement("TD"); 
+td.setAttribute("colspan", 1);  
+td.appendChild(document.createTextNode("2 ячейка")); 
+tr.appendChild(td); 
+td = document.createElement("TD"); 
+table.appendChild(tr);
+tr = document.createElement("TR"); 
+td = document.createElement("TD"); 
+td.appendChild(document.createTextNode("3 ячейка")); 
+tr.appendChild(td); 
+td = document.createElement("TD"); 
+td.appendChild(document.createTextNode("4 ячейка")); 
+tr.appendChild(td); 
+table.appendChild(tr);
+};
+createTable();
+
+function changeByClick() {
+table.click = function() {
+	if(table.classList.contains("highlight")) {
+		table.classList.replace("highlight", "bordered");
+	} else if (table.classList.contains("bordered")) {
+		table.classList.replace("bordered", "striped");
+	} else if (table.classList.contains("striped")) {
+		table.classList.replace("striped", "highlight");
+	} else {table.classList.add("bordered");
+}
+};
+}
+changeByClick();
+console.log(table.classList);
+table.click();
+console.log(table.classList);
+table.click();
+console.log(table.classList);
+table.click();
+console.log(table.classList);
 */
