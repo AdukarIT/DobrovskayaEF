318dd11de3de14ccf15bfb6e2a6eef165db1b268
DobrovskayaElena <44357557+DobrovskayaElena@users.noreply.github.com>
2019-03-10 15:55:04 +0300

    corrected

--- practice/practice DOM/scriptpracticeDOM.js
+++ practice/practice DOM/scriptpracticeDOM.js
@@ -29,12 +29,12 @@ document.getElementsByTagName("form")[1];
 
 Task 4. Форму с именем search, без использования её позиции в документе.
 
-document.querySelector("form[name="search"]");
+document.querySelector('form[name="search"]');
 
 
 Task 5. Элемент input в форме с именем search. Если их несколько, то нужен первый.
 
-document.querySelector("form[name="search"] input");
+document.querySelector('form[name="search"] input');
 
 
 Task 6. Элемент с именем info[0], без точного знания его позиции в документе.
@@ -44,5 +44,5 @@ document.getElementsByName("info[0]")[0];
 
 Task 7. Элемент с именем info[0], внутри формы с именем search-person.
 
-document.querySelector("form[name="search-person"] [name="info[0]"]");
+document.querySelector('form[name="search-person"] [name="info[0]"]');
 */
