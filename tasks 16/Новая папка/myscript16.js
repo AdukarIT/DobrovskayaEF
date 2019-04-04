b780b84d95481b7df3816560cb2e0dc26fbc97db
DobrovskayaElena <44357557+DobrovskayaElena@users.noreply.github.com>
2019-04-04 20:21:54 +0300

    corrected

--- tasks 16/Новая папка/myscript16.js
+++ tasks 16/Новая папка/myscript16.js
@@ -1,6 +1,6 @@
 $.getJSON("https://jsonplaceholder.typicode.com/albums", function(json) {
   $.each(json, function(index, obj) {
-    $("#albums").append($('<div>').html('<li class="newDiv"><a id="id" class="link" href="#" data-id="'+obj.id+'">'+obj.title+'</a></li>'))
+    $("#albums").append($('<ul>').html('<li id="id" class="link" data-id="'+obj.id+'">'+obj.title+'</li></ul>'))
   });
 });
 
@@ -8,11 +8,11 @@ $.getJSON("https://jsonplaceholder.typicode.com/albums", function(json) {
 $(document).on('click', '#id', function () {
   let album = $(this).attr("data-id");
   $.getJSON("https://jsonplaceholder.typicode.com/photos", function(json) {
-      $.each(json, function(index, obj) {
-    if(album == obj.albumId) {
-    $("#photos").append($('<div>').html('<li class="photos"><img src="'+this.url+'" data-id="'+obj.albumId+'"></li>'))
-  }
-})
+    $.each(json, function(index, obj) {
+      if(album == obj.albumId) {
+        $("#photos").append($('<div>').html('<p><img src="'+this.url+'" data-id="'+obj.albumId+'"></p></div>'))
+      }
+    })
   })
 })
 
