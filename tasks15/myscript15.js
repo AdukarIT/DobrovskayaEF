b854cca230a905f7daeafa628c4f71be575ab12d
DobrovskayaElena <44357557+DobrovskayaElena@users.noreply.github.com>
2019-04-07 00:49:25 +0300

    corrected

--- tasks15/myscript15.js
+++ tasks15/myscript15.js
@@ -1,28 +1,30 @@
-$(document).ready(function(){
-	$("#reg-form").validate({
-		rules:{
-			name:{
-            required: true,
-            minlength: 3,
-            maxlength: 20,
-        },
-        phone:{
-        	required: true,
-            digits: true,
-            minlength: 10,
-            maxlength: 12,
-        },
-            email:{
-            required: true,
-            email: true,
-        },
-        message:{
-        	required: true,
-            minlength: 10,
-            maxlength: 1000,
-        },
-    },
-    messages:{
-    }     
-});
-});
+$('#reg-form').submit(function () {
+	let name = $('#name').val();
+	let phone = $('#phone').val();
+	let email = $('#email').val();
+	let message = $('#message').val();
+	let emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	let phoneReg = /[0-9 -()+]+$/;
+	if ( name == "" ) {
+		alert('Введите имя');
+     	return false;
+	} else if ( phone == "" || (!phoneReg.test(phone)) ) {
+		$('#nameForm').css('display', 'block');
+		alert('Номер телефона введен некорректно');
+     	return false;
+	} else if ( email == "" || (!emailReg.test(email)) ) {
+		$('#phoneForm').css('display', 'block');
+		alert('Email введен некорректно');
+     	return false;
+     } else if ( message == "" ) {
+     	$('#emailForm').css('display', 'block');
+     	alert('Введите сообщение');
+     	return false;
+     } else if ( message.length < 10 || message.length > 1000 ) {
+     	alert('Сообщение должно быть от 10 до 1000 знаков');
+     	return false;
+     }
+     	else {
+     	$('#messageForm').css('display', 'block');
+     }
+ });
